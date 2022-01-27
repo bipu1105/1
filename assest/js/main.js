254ff0fa45686fb5d556410b00f4f5327b570b6d
bipu1105 <68435103+bipu1105@users.noreply.github.com>
2022-01-27 12:38:24 +0600

    1

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,7 +1,7 @@
 function fun(){
     let profile = document.querySelector('.testimonials .active img');
     let profileAll = document.querySelectorAll('.testimonials img');
-    let imgHeight = profile.scrollWidth;
+    let imgHeight = profile.clientWidth;
     profileAll.forEach(function(images){
         images.style.height = imgHeight + 'px'        
         images.style.padding = '10%';        
@@ -9,3 +9,22 @@ function fun(){
 }
 fun()
 window.addEventListener('resize' , ()=>{fun()})
+
+// // Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+    var forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+
